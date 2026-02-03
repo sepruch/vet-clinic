e7259b084a073a692153db5c0fff5faffe3761dd
Kirill <kirillsergienko2@gmail.com>
2026-02-03 14:58:22 +0300

    Initial commit: React frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <div style={{ padding: '16px' }}>
+            <h1>Ветеринарная клиника</h1>
+            <p>Frontend работает 🚀</p>
+        </div>
+    )
+}
+
+export default App
+
